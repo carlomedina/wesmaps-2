4e20dec8862007588d4b43bb914434c9829b95dd
Medina <lmedina@wesleyan.edu>
2018-03-21 01:38:43 -0400

    explored ways to get OD-matrix

--- 02-management.R
+++ 02-management.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(magrittr)
 library(lubridate)
+library(ggridges)
+library(migest)
 
 data <-  read_csv("./data/wesmaps_1169.csv")
 
@@ -87,9 +89,13 @@ data_fall <- data_clean %>%
   mutate(location = ifelse(location == "", last(location), location)) %>%
   filter(!is.na(start) & !ispoi) # remove pois and those with no start time
 
+
+
+#### COUNTING ####
+
 # by building
 counts_day_time_loc <- data_fall %>%
-  mutate(building = str_replace(location, "[0-9]+$", ""),
+  mutate(building = str_replace(location, "[0-9]+$", "") %>% str_trim(),
          day = factor(day, 
                       c("m", "t", "w", "r", "f"),
                       c("m", "t", "w", "r", "f"),
@@ -99,14 +105,190 @@ counts_day_time_loc <- data_fall %>%
   arrange(day, start, building) %>%
   ungroup() %>%
   mutate(start = paste(Sys.Date(), start) %>% ymd_hm(),
-         end = paste(Sys.Date(), end) %>% ymd_hm())
+         end = paste(Sys.Date(), end) %>% ymd_hm()) 
+
+### total_counts 
+top_building_list <- counts_day_time_loc %>%
+  group_by(building) %>%
+  summarise(count = sum(count)) %>%
+  arrange(desc(count)) %$%
+  head(building, 10)
+
+counts_day_time_loc %>%
+  filter(building %in% top_building_list) %>%
+  mutate(building = factor(building, rev(top_building_list), rev(top_building_list), ordered = T),
+         day = factor(day, c("m", "t", "w", "r", "f"), c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"), ordered = T)) %>%
+
+  ggplot() + 
+    geom_segment(aes(x = start, xend = end, y = building, yend = building, size = count), color = "#ba0c2f", alpha = 0.3) + 
+    scale_size_continuous(range = c(1, 15) ) +
+    scale_x_datetime() +
+    facet_wrap(~day, ncol = 5) +
+    theme_classic() +
+    labs(title = "Top 10 buildings with the greatest traffic",
+         subtitle = "Source: WesMaps",
+         y = "Building",
+         x = "Time",
+         size = "Volume") +
+  theme(plot.title = element_text(size = 20, face = "bold", family = "Source Sans Pro"),
+        axis.title = element_text(size = 14, face = "bold", family = "Source Sans Pro"),
+        axis.title.x = element_text(margin = margin(t = 20, l = 20)),
+        legend.title = element_text(size = 14, face = "bold", hjust = 1, family = "Source Sans Pro"),
+        legend.position = "bottom")
+
+# CREATE A WINDOW FUNCTION #
+window_aggregator <- function(data, start_time = "8:00am", end_time = "10:00pm", window_size = "5 min") {
+  init <- 0
+  standard_time <- seq(ymd_hm(paste(Sys.Date(), start_time)),
+                ymd_hm(paste(Sys.Date(), end_time)), by = window_size)
+  counts <- lapply(1:length(standard_time), function (index) {
+    point <- standard_time[index]
+    window <- data %>%
+      dplyr::filter(start <= point & end > point)
+    if (nrow(window) > 0) {
+      return(sum(window$count))
+    } else {
+      return(0)
+    }
+  }) %>%
+    unlist()
+  return(tibble(time = standard_time, counts = counts))
+}
+
+standardized <- counts_day_time_loc %>%
+  group_by(building, day) %>%
+  nest() %>%
+  mutate(standardized_time = map(data, window_aggregator)) %>%
+  unnest(standardized_time, .drop = T)
+
+standardized %>%
+  filter(building %in% top_building_list) %>%
+  mutate(building = factor(building, (top_building_list), (top_building_list), ordered = T),
+         day = factor(day, 
+                      rev(c("m", "t", "w", "r", "f")), 
+                      rev(c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")), 
+                      ordered = T)) %>%
+
+  ggplot() +
+  geom_ridgeline(aes(x = time, height = counts, y = day, fill = day, color = building), size = 0.01, scale = 0.001, stat = "identity", alpha = 0.3) +
+  # geom_line(aes(x = time, y = counts, color = building)) +
+  scale_fill_cyclical(values = c("#ba0c2f", "black")) +
+  scale_color_cyclical(values = c("blue", "green")) +
+  facet_wrap(~building, ncol = 10) + 
+  theme_classic() + 
+  theme(plot.title = element_text(size = 20, face = "bold", family = "Source Sans Pro"),
+        axis.title = element_text(size = 14, face = "bold", family = "Source Sans Pro"),
+        axis.title.x = element_text(margin = margin(t = 20, l = 20)),
+        legend.title = element_text(size = 14, face = "bold", hjust = 1, family = "Source Sans Pro"),
+        legend.position = "bottom")
+
+#### by major classes ####
+counts_day_time_major <- data_fall %>%
+  mutate(major = str_replace(id, "[0-9-]+$", "") %>% str_trim(),
+         day = factor(day, 
+                      c("m", "t", "w", "r", "f"),
+                      c("m", "t", "w", "r", "f"),
+                      ordered = T)) %>%
+  group_by(day, start, end, major) %>%
+  summarise(count = sum(enrolled)) %>%
+  arrange(day, start, major) %>%
+  ungroup() %>%
+  mutate(start = paste(Sys.Date(), start) %>% ymd_hm(),
+         end = paste(Sys.Date(), end) %>% ymd_hm()) 
+
+### total_counts 
+top_major_list <- counts_day_time_major %>%
+  group_by(major) %>%
+  summarise(count = sum(count)) %>%
+  arrange(desc(count)) %$%
+  head(major, 10)
+
+counts_day_time_major %>%
+  filter(major %in% top_major_list) %>%
+  mutate(major = factor(major, rev(top_major_list), rev(top_major_list), ordered = T),
+         day = factor(day, c("m", "t", "w", "r", "f"), c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"), ordered = T)) %>%
+  
+  ggplot() + 
+  geom_segment(aes(x = start, xend = end, y = major, yend = major, size = count), color = "#ba0c2f", alpha = 0.3) + 
+  scale_size_continuous(range = c(1, 15) ) +
+  scale_x_datetime() +
+  facet_wrap(~day, ncol = 5) +
+  theme_classic() +
+  labs(title = "Top 10 major classes with the greatest traffic",
+       subtitle = "Source: WesMaps",
+       y = "Major",
+       x = "Time",
+       size = "Volume") +
+  theme(plot.title = element_text(size = 20, face = "bold", family = "Source Sans Pro"),
+        axis.title = element_text(size = 14, face = "bold", family = "Source Sans Pro"),
+        axis.title.x = element_text(margin = margin(t = 20, l = 20)),
+        legend.title = element_text(size = 14, face = "bold", hjust = 1, family = "Source Sans Pro"),
+        legend.position = "bottom")
 
 
-ggplot(counts_day_time_loc) + 
-  geom_segment(aes(x = start, xend = end, y = building, yend = building, color = count, group = building), size = 2, alpha = 0.3) + 
-  facet_wrap(~day) +
-  theme_classic()
+
+standardized_major <- counts_day_time_major %>%
+  group_by(major, day) %>%
+  nest() %>%
+  mutate(standardized_time = map(data, window_aggregator)) %>%
+  unnest(standardized_time, .drop = T)
+
+standardized_major %>%
+  filter(major %in% top_major_list) %>%
+  mutate(major = factor(major, (top_major_list), (top_major_list), ordered = T),
+         day = factor(day, 
+                      rev(c("m", "t", "w", "r", "f")), 
+                      rev(c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")), 
+                      ordered = T)) %>%
+  
+  ggplot() +
+  geom_ridgeline(aes(x = time, height = counts, y = day, fill = day, color = major), size = 0.01, scale = 0.001, stat = "identity", alpha = 0.3) +
+  # geom_line(aes(x = time, y = counts, color = building)) +
+  scale_fill_cyclical(values = c("#ba0c2f", "black")) +
+  scale_color_cyclical(values = c("blue", "green")) +
+  facet_wrap(~major, ncol = 10) + 
+  theme_classic() + 
+  theme(plot.title = element_text(size = 20, face = "bold", family = "Source Sans Pro"),
+        axis.title = element_text(size = 14, face = "bold", family = "Source Sans Pro"),
+        axis.title.x = element_text(margin = margin(t = 20, l = 20)),
+        legend.title = element_text(size = 14, face = "bold", hjust = 1, family = "Source Sans Pro"),
+        legend.position = "bottom")
 
 
+## create function to get entry and exit counts
+entry_exit_counts <- function(data) {
+  time <- data$time
+  counts <- data$counts
+  entry_exit <- lapply(2:(length(time)-1), function (index) {
+    # just entered and about to leave
+    # if just entered and about to leave is negative, maket them zero
+    # the movement has been taken into account in time index-1
+    exit <- (counts[index+1] - counts[index]) %>% 
+      {ifelse(. > 0, 0, abs(.))}
+    entry <- (counts[index] - counts[index-1]) %>% 
+      {ifelse(. < 0, 0, .)}
+    return(list(entry = entry, exit = exit))
+  }) %>%
+    do.call('rbind.data.frame', .)
+  return(cbind.data.frame(time = time[2:(length(time)-1)], entry_exit))
+}
+
+entry_exit_loc <- standardized %>%
+  group_by(building, day) %>%
+  nest() %>%
+  mutate(entry_exit = map(data, entry_exit_counts)) %>%
+  unnest(entry_exit, .drop = T)
 
 
+# take some time window and see the number entry / exit data
+time <- "10:15am"
+astime <- ymd_hm(paste(Sys.Date(), time))
+window <- 1200
+
+temp <- entry_exit_loc %>%
+  filter(time < (astime + window) & time > (astime - window)) %>%
+  group_by(building) %>%
+  summarise(entry = sum(entry),
+            exit = sum(exit))
+
+colSums(temp[2:3])
