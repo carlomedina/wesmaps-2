8d62614d526a39c2c9c57cd8f499ec2a3ebb336c
Medina <lmedina@wesleyan.edu>
2018-03-21 02:49:42 -0400

    explored ways to visualize OD matrix

--- 02-management.R
+++ 02-management.R
@@ -3,7 +3,9 @@ library(magrittr)
 library(lubridate)
 library(ggridges)
 library(migest)
-
+library(network)
+library(ggraph)
+library(igraph)
 data <-  read_csv("./data/wesmaps_1169.csv")
 
 #### find a way to disentangle the time/location element
@@ -88,6 +90,17 @@ data_fall <- data_clean %>%
   select(-c(maxcap, available, sem)) %>%
   mutate(location = ifelse(location == "", last(location), location)) %>%
   filter(!is.na(start) & !ispoi) # remove pois and those with no start time
+
+
+################################################################
+#
+#
+#       EVERYTHING BELOW NEEDS REFACTORING!!!!!!
+#
+#
+################################################################
+
+
 
 
 
@@ -291,4 +304,192 @@ temp <- entry_exit_loc %>%
   summarise(entry = sum(entry),
             exit = sum(exit))
 
-colSums(temp[2:3])
+
+
+#### OD MATRIX ANALYSIS ####
+
+## HELPER FUNCTIONS ##
+# adapted from my mrt-eda exploration
+
+equalize_sum <- function(vector1, vector2) {
+  is_vector1_greater <- sum(vector1) >= sum(vector2) 
+  diff <- ifelse(
+    is_vector1_greater, 
+    sum(vector1) - sum(vector2),
+    sum(vector2) - sum(vector1)
+  )
+  print(sprintf("THE GAP BETWEEN ENTRANCE AND EXIT VOLUME IS: %s", diff))
+  print(sprintf("ERROR RATE: %s", ifelse(is_vector1_greater, diff/sum(vector2), diff/sum(vector1))))
+  prop <- ifelse(
+    rep(is_vector1_greater, length(vector1)), 
+    vector1 / sum(vector1),
+    vector2 / sum(vector2)
+  )
+  
+  if (is_vector1_greater) {
+    # increase counts of 2
+    df <- tibble(vector = vector2, prop = prop*diff) %>%
+      mutate(int = floor(prop), 
+             resid = prop - int,
+             index = 1:n())
+  } else {
+    # increase counts of 1
+    df <- tibble(vector = vector1, prop = prop*diff) %>%
+      mutate(int = floor(prop),
+             resid = prop - int,
+             index = 1:n())
+  }
+  # counts to allocate
+  allocate <- diff - sum(df$int)
+  df %>%
+    arrange(desc(prop)) %>%
+    mutate(newvector = vector + int + c(rep(1, allocate), rep(0, n()-allocate))) %>%
+    arrange(index) %$%
+    newvector -> newvector
+  
+  if (is_vector1_greater) {
+    return(list(vector1, newvector))
+  } else {
+    return(list(newvector, vector2))
+  }
+}
+
+
+flowplots <- function(data) {
+  data %$%
+    equalize_sum(entry, exit) -> net_counts
+  
+  mat <- net_counts %>%
+  {cm2(.[[1]], .[[2]])}
+  
+  mat <- mat[[1]] 
+  rownames(mat) <- data$building
+  colnames(mat) <- data$building
+  # # # mat <- scale(mat, center = F)
+  g <- graph_from_adjacency_matrix(mat, mode = "directed", weighted = T)
+  
+  edgelist <- get.edgelist(g) %>%
+    as.tibble() %>%
+    rename(from = V1, to = V2) %>%
+    mutate(from = factor(from),
+           to = factor(to),
+           fromindex = as.numeric(from),
+           toindex = as.numeric(to)) %>%
+    mutate(weight = E(g)$weight) %>%
+    arrange(from, to) %>%
+    group_by(from) %>%
+    mutate(curvature = seq(from = 0.5, by = 0.2, length.out = n()))
+  
+  g <- graph_from_data_frame(edgelist)
+  E(g)$curvature = edgelist$curvature
+  E(g)$weight = edgelist$weight
+  # plot(g,
+  #      vertex.size = 0.1,
+  #      vertex.label = NA,
+  #      edge.width = E(g)$weight*2,
+  #      edge.arrow.size = 0.01,
+  #      edge.color = E(g)$color,
+  #      layout = matrix(c(rep(0, 13), (1:13)*10), byrow = F, ncol = 2),
+  #      edge.curved=E(g)$curvature)
+  
+  # flow plot
+  p1 <- ggraph(g, layout = 'nicely') +
+    geom_edge_arc(aes(alpha = weight), 
+                  curvature = 1, 
+                  arrow = arrow(length = unit(2, 'mm'), 
+                                type = "closed")) +
+    scale_edge_width(range = c(0.5, 5)) +
+    scale_edge_alpha(guide = F) +
+    geom_node_text(aes(label = name)) +
+    scale_edge_color_discrete(guide = F) + 
+    # geom_node_text(aes(label = name)) +
+    theme_graph() +
+    scale_x_reverse() +
+    coord_flip() +
+    theme(legend.position = "none") +
+    labs(edge_width = "Volume")
+  print(p1)
+  # # heatmap
+  # rownames(mat) <- station_labels
+  # colnames(mat) <- station_labels
+  # heatmap_dat <- mat %>%
+  #   apply(1, function(x) {x/sum(x)}) %>%
+  #   t() %>%
+  #   melt() %>%
+  #   rename(from = Var1, to = Var2) %>%
+  #   mutate(indexto = rep(1:13, each = 13),
+  #          indexfrom = rep(1:13, 13),
+  #          from = factor(from, levels = rev(station_labels), ordered = T)) %>%
+  #   mutate(isSB = ifelse(indexfrom == indexto, NA, indexfrom < indexto),
+  #          value = ifelse(value == 0, NA, value))
+  # 
+  # p2 <- ggplot(heatmap_dat) +
+  #   geom_tile(aes(to, from, fill = value), alpha = 0.8, color = "white", size = 2) +
+  #   theme_classic() +
+  #   theme(legend.position = "right",
+  #         axis.text.x = element_text(angle = 45, hjust = 0, colour = "grey50"),
+  #         axis.title.x = element_text(hjust = 1, margin = margin(t = -15, unit = "pt"), size = 12),
+  #         axis.title.y = element_text(hjust = 1, margin = margin(b = 50, unit = "pt"), size = 12)) +
+  #   scale_x_discrete(position = "top") +
+  #   scale_fill_continuous(low = "grey90", high = "steelblue", na.value = "black", breaks = 0.05*0:5, labels = percent, limits = c(0, 0.25)) +
+  #   scale_color_discrete(na.value = "white") +
+  #   guides(alpha = FALSE) +
+  #   coord_fixed() +
+  #   labs(fill = "Percent traffic \nfrom origin \nto destination",
+  #        x = "Destination",
+  #        y = "Origin")
+  # 
+  # mintime_label <- paste0(str_pad(mintime, width = 2, side = "left", pad = "0"), "00")
+  # maxtime_label <- paste0(str_pad(maxtime, width = 2, side = "left", pad = "0"), "00")
+  # title <- cowplot::ggdraw() + 
+  #   cowplot::draw_label(sprintf("Passenger flow between %s and %s", mintime_label, maxtime_label), fontface='bold', hjust = 1, size = 16)
+  # cowplot::plot_grid(p1, p2, ncol = 2, rel_widths = c(0.3, 0.8), scale = c(1,1)) %>%
+  # {cowplot::plot_grid(title, ., ncol = 1, rel_heights = c(0.1, 1), rel_widths = c(1, 1))}
+}
+
+pdf("./output/checkflows.pdf")
+check_times <- c("10:15am", "2:45pm")
+for (time in check_times) {
+  # take some time window and see the number entry / exit data
+  astime <- ymd_hm(paste(Sys.Date(), time))
+  window <- 1200
+  
+  temp <- entry_exit_loc %>%
+    filter(time < (astime + window) & time > (astime - window)) %>%
+    group_by(building) %>%
+    summarise(entry = sum(entry),
+              exit = sum(exit)) %>%
+    arrange(entry)
+  flowplots(temp)
+}
+dev.off()
+
+
+temp %$%
+  equalize_sum(entry, exit) -> net_counts
+
+mat <- net_counts %>%
+{cm2(.[[1]], .[[2]])}
+
+mat <- mat[[1]] 
+rownames(mat) <- temp$building
+colnames(mat) <- temp$building
+chordDiagram(mat, 
+             transparency = 0.4, 
+             annotationTrack = c("grid"),
+             preAllocateTracks=list(track.height = 0.1))
+circos.trackPlotRegion(track.index=1, 
+                       panel.fun=function(x,y) {
+                        xlim = get.cell.meta.data("xlim") 
+                        ylim = get.cell.meta.data("ylim")
+                        sector.name=get.cell.meta.data("sector.index")
+                        
+                        circos.text(mean(xlim), 
+                          ylim[1], 
+                          sector.name,
+                          facing="clockwise",
+                          niceFacing=TRUE,
+                          adj=c(0,0.5),
+                          cex = 0.5)
+                      },
+                      bg.border=NA)
