00f1421b5370d3b76736276f88d9578857e18d46
carlo <lmedina@wesleyan.edu>
2018-03-28 02:17:36 -0400

    fixed building addresses manually

--- 05-building_location.R
+++ 05-building_location.R
@@ -8,14 +8,24 @@ read_html(building_url) %>%
   .[[1]] %>% 
   as.matrix() %>%
   {rbind(.[,1:2], .[,3:4])} %>%
-  as.data.frame() %>%
+  as.data.frame(stringsAsFactors = F) %>%
   {tibble(code = .$X1[.$X1 != ""],
          name = .$X2[seq(1, nrow(.), 2)],
          address = .$X2[seq(2, nrow(.), 2)])} -> buildings
 
-buildings_geocoded <- buildings %>%
+# correct the mistakes
+buildings$address[buildings$code == "CRT"] <- "285 Court St"
+buildings$address[buildings$code == "92TH"] <- "207 High St"
+buildings$address[buildings$code == "VVO"] <- "96 Foss Hill"
+buildings$address[buildings$code == "SHAN"] <- "237 Church St"
+buildings$address[buildings$code == "TST"] <- "271 Washington Terrace"
+buildings$address[buildings$code == "ZLKA"] <- "283 Washington Terrace"
+buildings$address[buildings$code == "HALL"] <- "52 Lawn Avenue"
+
+# add geocoding
+buildings %>%
   mutate(location = sprintf("%s, Middletown, CT", address)) %>%
-  mutate_geocode(location = location, source = "dsk")
+  mutate_geocode(location = location, source = "dsk", force = T, override_limit = T) -> buildings_geocoded
   
 write_csv(buildings_geocoded, "./data/building_location.csv")
 
